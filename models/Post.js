7a1cf1e4181c31eef6f683cee8d1a11302cbd78e
Brian Done <briguy2078@gmail.com>
2022-02-07 16:41:48 -0500

    added post and comment classes

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+// Post Class
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING(2000),
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING(2000),
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
