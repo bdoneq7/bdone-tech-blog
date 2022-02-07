ee29d51a96ef889ec7b078929eb2a7e410a8c8ba
Brian Done <briguy2078@gmail.com>
2022-02-07 15:56:27 -0500

    updated auth file for user login and redirect

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+// Requires User to be Logged In - Otherwise Redirects to Login 
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
